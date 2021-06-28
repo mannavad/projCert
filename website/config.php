0c7ce475c72329300ba48461f23fce6ed54ebd83
dhana mannava <dhanamannava@gmail.com>
2021-06-28 22:28:03 +0530

    trying to commit in another branch

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple Dhana PHP Website',
+        'name' => 'Simple Dhanalaxmi PHP Website',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
