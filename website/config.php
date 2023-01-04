16925c732f285064e41b7ed56eef10660002e504
mannavad <82090110+mannavad@users.noreply.github.com>
2023-01-04 11:34:22 +0530

    Update config.php

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple Dhana PHP Website',
+        'name' => 'Simple phani PHP Website',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
